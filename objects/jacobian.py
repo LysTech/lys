jacobian looks fine when sqrt(jacobian) is plotted, fixed 1,2 axis in sample_at_vertices func,

objects/jacobian.py
@@ -287,8 +287,10 @@ def jacobian_to_vertex_val(J_vert: np.ndarray, mode: str = 'fro') -> np.ndarray:
         ValueError: If mode is not 'fro' or 'max'.
     """
     if mode == 'fro':
-        return np.linalg.norm(J_vert, axis=(0, 1))
+        return np.linalg.norm(J_vert, axis=(1, 2))
     elif mode == 'max':
-        return np.abs(J_vert).max(axis=(0, 1))
+        return np.abs(J_vert).max(axis=(1, 2))
+    elif mode == "sum":
+        return np.sum(J_vert, axis = (1, 2))
     else:
         raise ValueError(f"Invalid mode '{mode}'. Must be 'fro' or 'max'.")

processing/steps.py
@@ -74,8 +74,8 @@ class ReconstructDual(ProcessingStep):
         Bmn_wl2 = self.compute_Bmn(vertex_jacobian_wl2, phi)
 
         # Get t-stat data
-        t_HbO_data = session.processed_data["t_HbO"]
-        t_HbR_data = session.processed_data["t_HbR"]
+        t_HbO_data = session.processed_data["wl1"]
+        t_HbR_data = session.processed_data["wl2"]
         
         # Initialize reconstructed data dictionaries
         session.processed_data["t_HbO_reconstructed"] = {}
@@ -108,8 +108,8 @@ class ReconstructDual(ProcessingStep):
             session.processed_data["t_HbR_reconstructed"][task] = reconstructed_HbO  # Note: reconstruct_dual only returns HbO
         
         # Clean up intermediate data
-        del session.processed_data["t_HbO"]
-        del session.processed_data["t_HbR"]
+        del session.processed_data["wl1"]
+        del session.processed_data["wl1"]
 
     def get_best_reg_param_dual(self, Bmn_wl1, Bmn_wl2, y_sd, y_sd_HbR, phi, eigenvals, vertices, fmri_tstats):
         """
@@ -630,6 +630,42 @@ class RemoveScalpEffect(ProcessingStep):
 
         return idx_start, idx_end
 
+class HemoToOD(ProcessingStep):
+    def hemo_to_od(self,
+            HbO,
+            HbR,
+    ):
+        T = len(HbO)
+        HbO = np.reshape(HbO, (T,num_sources,num_detectors))
+        HbR = np.reshape(HbR, (T, num_sources, num_detectors))
+        S = num_sources
+        D = num_detectors
+        od_wl1 = np.zeros((T, S, D))
+        od_wl2 = np.zeros((T, S, D))
+        for t in range(T):
+            for s_idx in range(S):
+                for d_idx in range(D):
+                    hemo_vec = np.array([HbO[t, s_idx, d_idx], HbR[t, s_idx, d_idx]])
+                    od_vec = A.dot(hemo_vec)
+                    od_wl1[t, s_idx, d_idx] = od_vec[0]
+                    od_wl2[t, s_idx, d_idx] = od_vec[1]
+
+        return od_wl1, od_wl2
+
+    def _do_process(self, session: 'Session') -> None:
+        HbO = session.processed_data["t_HbO"]
+        HbR = session.processed_data["t_HbR"]
+        od_wl1, od_wl2 = self.hemo_to_od(HbO,HbR)
+        session.processed_data["wl1"] = od_wl1
+        session.processed_data["wl2"] = od_wl2
+        del session.processed_data["t_HbO"]
+        del session.processed_data["t_HbR"]
+
+
+# class Zscore(ProcessingStep):
+#     def _do_process(self, session: 'Session') -> None:
+#         processed_data = session.processed_data("HbO)
+#         processed_data = processed_data - np.mean(processed_data)
 
 class BandpassFilter(ProcessingStep):
     """

research/bettina_analysis.py
@@ -6,25 +6,25 @@ from lys.objects.mesh import StaticMeshData
 from lys.objects.jacobian import jacobian_to_vertex_val
 from lys.processing.pipeline import ProcessingPipeline
 
-experiment_name = "fnirs_8classes"
+experiment_name = "8classes"
 experiment = create_experiment(experiment_name, "nirs")
-#experiment.sessions = experiment.sessions[0:1]
+experiment.sessions = experiment.sessions[0:1]
 
 
-if False:
-    """ Check mesh and volume alignmnent """
-    mesh = experiment.sessions[0].patient.mesh
-    segmentation = experiment.sessions[0].patient.segmentation
-    scene = VTKScene(title="Mesh and segmentation alignment")
-    # See how our alignment is not perfect :( !! 
-    scene.add(mesh).add(segmentation).format(segmentation, opacity=0.02).show()
+#if False:
+""" Check mesh and volume alignmnent """
+mesh = experiment.sessions[0].patient.mesh
+segmentation = experiment.sessions[0].patient.segmentation
+scene = VTKScene(title="Mesh and segmentation alignment")
+# See how our alignment is not perfect :( !!
+scene.add(mesh).add(segmentation).format(segmentation, opacity=0.02).show()
 
-    """ Check projected Jacobian """
-    scene = VTKScene(title="Projected Jacobian")
-    sd_vertex_jacobian_wl1 = experiment.sessions[0].jacobians[0].sample_at_vertices(mesh.vertices)
-    vertex_jacobian_wl1 = jacobian_to_vertex_val(sd_vertex_jacobian_wl1)
-    sd_mesh = StaticMeshData(mesh, vertex_jacobian_wl1)
-    scene.add(sd_mesh).add(segmentation).format(segmentation, opacity=0.02).show()
+""" Check projected Jacobian """
+scene = VTKScene(title="Projected Jacobian")
+sd_vertex_jacobian_wl1 = experiment.sessions[0].jacobians[0].sample_at_vertices(mesh.vertices)
+vertex_jacobian_wl1 = jacobian_to_vertex_val(sd_vertex_jacobian_wl1, mode = "sum")
+sd_mesh = StaticMeshData(mesh, np.sqrt(vertex_jacobian_wl1))
+scene.add(sd_mesh).add(segmentation).format(segmentation, opacity=0.02).show()
 
 
 config = [
@@ -32,6 +32,9 @@ config = [
     {"ConvertODtoHbOandHbR": {}},
     {"RemoveScalpEffect": {}},
     {"ConvertToTStats": {}},
+    {"HemoToOD":{}},
+#    {"Zscore":{}},
+#    {"ConvertToTStats": {}},
     {"ReconstructDual": {"num_eigenmodes": 390}}
 ]
 
@@ -40,7 +43,7 @@ experiment = processing_pipeline.apply(experiment)
 
 
 """ Check correlations """
-from lys.utils.mri_tstat import get_mri_tstats 
+from lys.utils.mri_tstat import get_mri_tstats
 tstat_firsts = []
 for session in experiment.sessions:
     for task in session.protocol.tasks: